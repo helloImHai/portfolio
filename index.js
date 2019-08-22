73e4424816724c9332bf4042ff31d99ad7012482
helloImHai <1503.hai.nguyen@gmail.com>
2019-08-23 00:16:07 +0800

    Fixed refresh error

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(express.static(path.join(__dirname, '/dist')));
+// app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'), (err) => {
+    if (err) {
+      console.log("error my guy");
+      res.status(500).send(err);
+    }
+  })
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
